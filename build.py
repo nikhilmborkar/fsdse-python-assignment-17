2db209d33edb25e7133d56b479e19409e297a272
nikhilmborkar <nikhilmukundborkar@gmail.com>
2017-06-26 01:32:28 +0000

    Done

--- build.py
+++ build.py
@@ -6,9 +6,11 @@ def solution_asc(dic):
     Enter your code here
     '''
 
+    return sorted(dic.items())
+
 
 def solution_desc(dic):
     '''
     Enter your code here
     '''
-
+    return sorted(dic.items(),reverse=True)
